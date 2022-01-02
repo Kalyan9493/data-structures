ac07fc714dbfa219222cfce225d63422ec19540f
Kalyan <kalyankumarreddy098@gmail.com>
2022-01-02 17:49:33 +0530

    Insertion sort added

---
+++ datastructures/src/main/java/datastructures/datastructures/sorting/InsertionSort.java
@@ -0,0 +1,35 @@
+package datastructures.datastructures.sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		int[] arr = {5,2,3,4,1,-1,-13};
+		System.out.println("Before Sorting : " + Arrays.toString(arr));
+		System.out.println("After Sorting : " + Arrays.toString(insertionSort(arr)));
+	}
+	
+	public static int[] insertionSort(int[] arr) {
+		// Correct but not make sense  for me
+//        for (int i = 1; i < arr.length; i++) {
+//            int j = i - 1;
+//            while (j >= 0 && arr[j] > arr[i]) {
+//                arr[j + 1] = arr[j];
+//                j--;
+//            }
+//            arr[j + 1] = arr[i];
+//        }
+		for(int i=0; i<arr.length-1; i++) {
+			for(int j=i+1; j>0; j--) {
+				if(arr[j] < arr[j-1]) {
+					arr[j] = arr[j] + arr[j-1] - (arr[j-1] = arr[j]);
+				}else {
+					break;
+				}
+			}
+		}
+		return arr;
+	}
+
+}
